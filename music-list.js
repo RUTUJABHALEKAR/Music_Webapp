e1215d96632bd653f306a3dea2675e9e75fe1c82
RUTUJABHALEKAR <99064660+RUTUJABHALEKAR@users.noreply.github.com>
2023-08-27 22:04:52 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,46 @@
+let allMusic = [
+
+    {
+        name: "Raataan Lambiyan",
+        artist: "Tanishk Bagchi",
+        img: "raataanLambiyan",
+        src: "raataanLambiyan"
+    },
+    {
+        name: "Kesariya",
+        artist: "Pritam",
+        img: "kesariya",
+        src: "kesariya"
+    },
+    {
+        name: "Ghungroo",
+        artist: "Vishal-Shekhar",
+        img: "ghungroo",
+        src: "ghungroo"
+    },
+    {
+        name: "Mera Wala Dance",
+        artist: "Nakash Aziz",
+        img: "meraWalaDance",
+        src: "meraWalaDance"
+    },
+    {
+        name: "What Jhumka ?",
+        artist: "Pritam",
+        img: "whatJhumka",
+        src: "whatJhumka"
+    },
+    {
+        name: "Tera Yaar Hoon Main",
+        artist: "Rochak Kohli",
+        img: "teraYaarHoonMain",
+        src: "teraYaarHoonMain"
+    },
+    {
+        name: "Param Sundari",
+        artist: "A.R. Rahman",
+        img: "paramSundari",
+        src: "paramSundari"
+    },
+    
+    ];
